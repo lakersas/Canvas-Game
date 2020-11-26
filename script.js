4e2f7af664580e353600683e37c8c670fd2e709b
Gintaras Puleikis <74766302+lakersas@users.noreply.github.com>
2020-11-26 21:20:06 +0200

    Mouse movement and game start

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+class Player {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.width = 50;
+        this.height = 50;
+    }
+
+    setPosition(x, y) {
+        this.x = x - (this.width / 2);
+        this.y = y - (this.height / 2);
+    }
+
+    draw() {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
 class Asteroid {
     constructor(size, speed) {
         this.size = size;
@@ -22,7 +40,9 @@ class Asteroid {
 const gameData = {
     points: 0,
     asteroids: [],
-    previousFrameTime: 0
+    previousFrameTime: 0,
+    player: new Player(),
+    gameStart: false
 };
 
 function frame() {
@@ -52,17 +72,49 @@ function updateGameData() {
 }
 
 function drawFrame() {
-    for (let asteroid of gameData.asteroids) {
-        asteroid.draw();
+    if (gameData.gameStart === false) {
+        ctx.font = "36px Apple Chancery, cursive";
+        ctx.textAlign = "center";
+        ctx.fillText("Click to Start Game", 400, 300);
     }
+
+    if (gameData.gameStart === true)
+        gameData.player.draw();
 }
 
 // Object parameters
-function startGame() {
+function initGame() {
+    document.addEventListener("click", startGame, true);
+    document.addEventListener("mousemove", mouseMovementHandler);
     gameData.asteroids.push(new Asteroid(50, 120));
 
     frame();
 }
 
+function startGame(e) {
+    gameData.gameStart = true;
+
+    const mousePosition = getMousePosition(e)
+    gameData.player.setPosition(mousePosition.x, mousePosition.y);
+}
+
+//Mouse movement fuction
+function mouseMovementHandler(e) {
+    const mousePosition = getMousePosition(e);
+
+    // Player mouse position
+    gameData.player.setPosition(mousePosition.x, mousePosition.y);
+}
+
+function getMousePosition(event) {
+    let relativeX = event.clientX - ctx.canvas.getBoundingClientRect().left;
+    let relativeY = event.clientY - ctx.canvas.getBoundingClientRect().top;
+
+    return {
+        x: relativeX,
+        y: relativeY
+    }
+}
+
 // Game Start
-startGame();
+initGame();
