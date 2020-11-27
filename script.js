f23df56bf7231788ac29caf8060e6312773eb331
Gintaras Puleikis <74766302+lakersas@users.noreply.github.com>
2020-11-27 18:53:01 +0200

    No progress

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+let enemyPosition = 0;
 
+// Class for every object in the game
 class Player {
     constructor() {
         this.x = 0;
@@ -37,14 +39,35 @@ class Asteroid {
     }
 }
 
+class enemyAsteroid {
+    constructor(size, speed) {
+        this.size = size;
+        this.speed = speed;
+        this.x = 0;
+        this.y = 0;
+        this.friendly = false;
+    }
+
+    update(deltaTime) {
+        this.x += deltaTime * this.speed;
+    }
+
+    draw() {
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+}
+
+// In game objects , enemy and player
 const gameData = {
     points: 0,
     asteroids: [],
     previousFrameTime: 0,
     player: new Player(),
-    gameStart: false
+    gameStart: false,
+    enemy: new enemyAsteroid()
 };
 
+// Display In Game
 function frame() {
     updateGameData();
     clearFrame();
@@ -53,10 +76,12 @@ function frame() {
     window.requestAnimationFrame(frame);
 }
 
+// ClearFrame - clears old frames so they don't stack in game
 function clearFrame() {
     ctx.clearRect(0, 0, 800, 600);
 }
 
+// A frame refers to the image we see on the screen, which gets updated a certain number of times a second. The frequency in which the frame is updated is known as frame rate, usually measured in frames per second (fps).
 function updateGameData() {
     const currentTime = Date.now();
     let deltaTime = (currentTime - gameData.previousFrameTime) / 1000;
@@ -68,9 +93,12 @@ function updateGameData() {
         asteroid.update(deltaTime);
     }
 
+    enemyPosition++;
+
     gameData.previousFrameTime = currentTime;
 }
 
+// Draws whats in game
 function drawFrame() {
     if (gameData.gameStart === false) {
         ctx.font = "36px Apple Chancery, cursive";
@@ -80,25 +108,32 @@ function drawFrame() {
 
     if (gameData.gameStart === true)
         gameData.player.draw();
+
+    // just to see if objects appear , need to make them in game.
+    ctx.fillRect(enemyPosition, 200, 100, 100);
+    ctx.fillRect(50, enemyPosition, 100, 100);
+    ctx.fillRect(200, enemyPosition, 100, 100);
+    ctx.fillRect(enemyPosition, 30, 100, 100);
+
 }
 
 // Object parameters
 function initGame() {
     document.addEventListener("click", startGame, true);
     document.addEventListener("mousemove", mouseMovementHandler);
-    gameData.asteroids.push(new Asteroid(50, 120));
-
     frame();
 }
 
+// Game start on click and object appearance on same spot there was the mouse
 function startGame(e) {
     gameData.gameStart = true;
 
     const mousePosition = getMousePosition(e)
     gameData.player.setPosition(mousePosition.x, mousePosition.y);
+
 }
 
-//Mouse movement fuction
+// Mouse movement function
 function mouseMovementHandler(e) {
     const mousePosition = getMousePosition(e);
 
@@ -106,6 +141,7 @@ function mouseMovementHandler(e) {
     gameData.player.setPosition(mousePosition.x, mousePosition.y);
 }
 
+// Object will be indicated where is players mouse
 function getMousePosition(event) {
     let relativeX = event.clientX - ctx.canvas.getBoundingClientRect().left;
     let relativeY = event.clientY - ctx.canvas.getBoundingClientRect().top;
